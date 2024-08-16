06feab56e6c1376eca87cb08b587aa995bbf62ec
Muhammet Eren <115183696+Merenylmz@users.noreply.github.com>
2024-08-16 09:39:48 +0300

    Demo

--- screens/MealsOverview.js
+++ screens/MealsOverview.js
@@ -1,14 +1,23 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { MEALS } from "../data/dummy-data";
+import {useLayoutEffect} from "react";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
 import MealItem from "../components/MealItem";
 
-const MealsOverview = ({route}) => {
+
+const MealsOverview = ({route, navigation}) => {
     const catId = route.params.categoryId;
 
     const displayedMeals = MEALS.filter((mealItem)=>{
         return mealItem.categoryIds.indexOf(catId) >= 0;
     })
+    
+    
+    useLayoutEffect(()=>{
+        const categoryTitle = CATEGORIES.find(category=>category.id == catId).title; 
+        navigation.setOptions({
+            title: categoryTitle
+        });
+    }, [catId, navigation]);
     
     
     const renderMealItem = (itemData) =>{
